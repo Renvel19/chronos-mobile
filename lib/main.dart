1e5ed48d299c3d24cf014dddff2f618769b1656d
Renvel19 <af.renteria2@uniandes.edu.co>
2025-03-05 20:23:41 -0500

    add login

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:chronos_mobile/config/router/app_router.dart';
+import 'package:chronos_mobile/config/theme/app_theme.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme().getTheme(),
+      routerConfig: appRouter,
+    );
+  }
+}
